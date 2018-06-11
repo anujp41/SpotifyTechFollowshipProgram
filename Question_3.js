5e5585b087fc4f97746cc155cf43ec954ea05928
anujp41 <anuj.pant41@gmail.com>
2018-06-11 12:00:54 -0400

    updated formatting of if-else statement in Q3

--- Question_3.js
+++ Question_3.js
@@ -3,8 +3,11 @@
 function changePossibilities(amount, denominations) {
   const result = [];
   for (let i = 0; i <= amount; i++) {
-    if (i === 0) result[0] = 1 //if target amount is 0, then there is 1 way of returning that amount; as this is always, we set the first value of result to 1
-    else result[i] = 0;
+    if (i === 0) {
+      result[0] = 1 //if target amount is 0, then there is 1 way of returning that amount; as this is always, we set the first value of result to 1
+    } else {
+      result[i] = 0;
+    }
   }
 
   for (let j = 0; j <= denominations.length; j++) {
